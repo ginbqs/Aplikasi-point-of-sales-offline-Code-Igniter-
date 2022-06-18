438abd7dc422424569e2841c93b3a5880a5d665c
ginanjar abdul wahab <gin.bqs@gmail.com>
2022-06-18 21:51:52 +0700

    perbaikan

--- app/Views/admin/layouts/index.php
+++ app/Views/admin/layouts/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>next-In | WarungKu</title>
+  <title>Ruang Inovator | WarungKu</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
--- app/Views/admin/layouts/sidebar.php
+++ app/Views/admin/layouts/sidebar.php
@@ -6,7 +6,7 @@
       <img src="<?php echo base_url('adminlte/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
     </div>
     <div class="info">
-      <a href="#" class="d-block">next-in</a>
+      <a href="#" class="d-block">Ruang Inovator</a>
     </div>
   </div>
 
